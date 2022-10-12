02bae07887ab2257695d5b86c69a33bff85fa41a
kepisqo <52206026+kepisqo@users.noreply.github.com>
2022-10-12 10:56:42 +0300

    commit

---
+++ js/about.7adaec33.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfrontend"]=self["webpackChunkfrontend"]||[]).push([[443],{2129:function(n,t,e){e.r(t),e.d(t,{default:function(){return i}});var r=e(3396);const u={class:""},s=(0,r._)("h3",null,"test",-1),c=[s];function f(n,t,e,s,f,a){return(0,r.wg)(),(0,r.iD)("div",u,c)}var a={},o=e(89);const d=(0,o.Z)(a,[["render",f]]);var i=d}}]);
+//# sourceMappingURL=about.7adaec33.js.map
